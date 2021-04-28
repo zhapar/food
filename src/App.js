946c5158922061451252e3d29d675f9869c8f157
Baistan Tashkuov <baistan.tashkuov_2025@ucentralasia.org>
2021-04-29 02:26:56 +0600

    [fix] App

--- src/App.js
+++ src/App.js
@@ -1,21 +1,5 @@
-<<<<<<< HEAD
-import React from "react";
-import Button from "./components/Button";
-import Card from "./components/Card";
-import Input from "./components/Input";
-
-import { ReactComponent as SearchIcon } from "./icons/search.svg";
-
-function App() {
-  return (
-    <div className="flex items-center justify-center h-screen">
-      {/* <Button type="primary">Confirm Payment</Button> */}
-      {/* <Input placeholder="Search" icon={<SearchIcon />} /> */}
-      <Card icon="card" name="Credit Card" />
-      <Card icon="paypal" name="PayPal" />
-=======
-import Sidebar from './features/sidebar'
-import { Switch, Route } from 'react-router-dom'
+import Sidebar from "./features/sidebar";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
@@ -24,9 +8,8 @@ function App() {
       <Switch>
         <Route path="/" exact></Route>
       </Switch>
->>>>>>> dfa8c2f82d48cfb212b393b6a5f6b595c9f08304
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
