ecaa1b02a5f79bed061572bc3c3472a55e2b6b27
Baistan Tashkuov <baistan.tashkuov_2025@ucentralasia.org>
2021-04-29 03:07:59 +0600

    [Modify] card component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Sidebar from "./features/sidebar";
 import { Switch, Route } from "react-router-dom";
+import Payment from "./features/payment/payment";
 
 function App() {
   return (
     <div className="flex">
       <Sidebar />
       <Switch>
-        <Route path="/" exact></Route>
+        <Route path="/" exact>
+          <Payment />
+        </Route>
       </Switch>
     </div>
   );
