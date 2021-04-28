5ce51d56009410824c9c2c527f4ba0f6c4b7e0bf
Baistan Tashkuov <baistan.tashkuov_2025@ucentralasia.org>
2021-04-29 00:43:32 +0600

    Button component added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import logo from "./logo.svg";
+import React from "react";
+import Button from "./components/Button";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div className="">
+      <Button type="secondary">Cancel</Button>
+    </div>
+  );
 }
 
 export default App;
